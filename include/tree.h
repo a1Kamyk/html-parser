f419bc6bb0c7002fec4636e250dc5074b04431fb
a1Kamyk <piotr.kamianka@onet.pl>
2025-11-18 13:02:04 +0100

    changed tree node struct to contain more information

--- include/tree.h
+++ include/tree.h
@@ -1,19 +1,47 @@
 #ifndef HTML_PARSER_TREE_H
 #define HTML_PARSER_TREE_H
 
-#include <stdint.h>
-
 #include "tokenizer.h"
 
-typedef struct dom_tree_node {
-    struct dom_tree_node **children;
-    size_t children_amount;
-    size_t children_capacity;
+/// Type of stored node
+typedef enum {
+    DOM_ROOT,
+    DOM_ELEMENT,
+    DOM_TEXT,
+    DOM_COMMENT,
+    DOM_DOCTYPE
+} dom_node_type_t;
 
-    token_t* token;
+typedef struct {
+    char* tag_name;
+    // TODO implement attributes
+    // attribute_t* attributes;
+    size_t attribute_count;
+} element_node_t;
+
+typedef struct {
+    char* text;
+} comment_node_t;
+
+/// Data of stored node
+typedef union {
+    element_node_t element;
+    comment_node_t comment;
+} node_data_t;
+
+typedef struct dom_tree_node {
+    /// Self data
+    dom_node_type_t type;
+    node_data_t     data;
+
+    /// Parent and children data
+    dom_tree_node_t*    parent;
+    dom_tree_node_t**   children;
+    size_t              children_amount;
+    size_t              children_capacity;
 } dom_tree_node_t;
 
-dom_tree_node_t *create_tree_node(void* data);
-void add_child(dom_tree_node_t *node, dom_tree_node_t *child);
+dom_tree_node_t *create_tree_node(token_t* token);
+void add_child(dom_tree_node_t *parent, dom_tree_node_t *child);
 
 #endif //HTML_PARSER_TREE_H
--- src/tree.c
+++ src/tree.c
@@ -3,37 +3,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "tokenizer.h"
+
+static const size_t INITIAL_CHILD_CAPACITY = 4;
 static const size_t DYN_ARR_EXPANSION_COEFFICIENT = 2;
 
-dom_tree_node_t *create_tree_node(void* data) {
+dom_tree_node_t *create_tree_node(token_t* token) {
     dom_tree_node_t *node = calloc(1, sizeof(dom_tree_node_t));
     if (!node) {
-        printf("Malloc failed: %i", errno);
+        fprintf(stderr, "Out of memory allocating new tree node");
         return NULL;
     }
 
-    node->data = data;
-    data = NULL;
+    // TODO only create a children array if node not a text or comment type
+    node->children = calloc(INITIAL_CHILD_CAPACITY, sizeof(dom_tree_node_t*));
+    if (!node->children) {
+        fprintf(stderr, "Out of memory allocating new child array");
+        return NULL;
+    }
+    node->children_capacity = INITIAL_CHILD_CAPACITY;
+    node->children_amount = 0;
+
+    // TODO fix this basically
+    switch (token->type) {
+        case DOCTYPE:
+        case START_TAG:
+        case END_TAG:
+        case COMMENT:
+        case CHARACTER:
+        case END_OF_FILE:
+        default:
+            break;
+    }
 
     return node;
 }
 
 
-void add_child(dom_tree_node_t *node, dom_tree_node_t *child) {
-    if (node->children_amount + 1 <= node->children_capacity) {
-        node->children[node->children_amount++] = child;
-        return;
-    }
+void add_child(dom_tree_node_t *parent, dom_tree_node_t *child) {
+    if (parent->children_amount + 1 > parent->children_capacity) {
+        if (parent->children_capacity == 0)
+            parent->children_capacity = 4;
+        else
+            parent->children_capacity *= DYN_ARR_EXPANSION_COEFFICIENT;
 
-    dom_tree_node_t **new_arr = realloc(node->children,
-                            node->children_capacity * sizeof(dom_tree_node_t *) * DYN_ARR_EXPANSION_COEFFICIENT
-                            );
-    if (!new_arr) {
-        printf("Failed adding child node");
-        return;
-    }
+        dom_tree_node_t **new_arr = realloc(
+            parent->children,
+    parent->children_capacity * sizeof(dom_tree_node_t*)
+        );
 
-    node->children = new_arr;
-    node->children_capacity *= DYN_ARR_EXPANSION_COEFFICIENT;
-    node->children[node->children_amount++] = child;
+        if (!new_arr) {
+            fprintf(stderr, "Out of memory allocating child array\n");
+            return;
+        }
+        parent->children = new_arr;
+    }
+    parent->children[parent->children_amount++] = child;
+    child->parent = parent;
 }
