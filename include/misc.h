edbc5dea31e475d847a38a85a40ec15c8e3190a9
a1Kamyk <piotr.kamianka@onet.pl>
2025-11-20 00:07:45 +0100

    added more string_t helper functions

--- include/misc.h
+++ include/misc.h
@@ -3,22 +3,23 @@
 
 #include <stdlib.h>
 #include <stdbool.h>
-#include <stdint.h>
 
 #define DEFAULT_CAPACITY 8
 #define CAPACITY_EXPANSION_COEF 2
 
-inline uint64_t parser_umax(const uint64_t a, const uint64_t b) {
-    return a > b ? a : b;
+#define parser_max(a, b) (a > b ? a : b)
+#define parser_min(a, b) (a < b ? a : b)
+
+static inline bool is_ascii_upper_alpha(const int c) {
+    return (0x0041 <= c && c <= 0x005A);
 }
-inline int64_t parser_max(const int64_t a, const int64_t b) {
-    return a > b ? a : b;
+
+static inline bool is_ascii_lower_alpha(const int c) {
+    return (0x0061 <= c && c <= 0x007A);
 }
-inline uint64_t parser_umin(const uint64_t a, const uint64_t b) {
-    return a < b ? a : b;
-}
-inline int64_t parser_min(const int64_t a, const int64_t b) {
-    return a < b ? a : b;
+
+static inline bool is_ascii_alpha(const int c) {
+    return is_ascii_upper_alpha(c) || is_ascii_lower_alpha(c);
 }
 
 typedef struct {
@@ -27,10 +28,14 @@ typedef struct {
     size_t capacity;
 } string_t;
 
+int parser_string_init(string_t* str);
+int parser_string_init_sized(string_t* str, size_t capacity);
+int parser_string_clear(string_t* str);
+int parser_string_delete(string_t* str);
 int parser_strcmp(const string_t* str1, const string_t* str2);
-int parser_cstrcmp(const char* str1, const string_t* str2)
-bool contains(const char* arr[], size_t arr_size, const string_t* str);
+int parser_cstrcmp(const char* str1, const string_t* str2);
+int parser_string_append_char(string_t* str, int c);
 
-int append_char(string_t* str, int c);
+bool contains(const char* arr[], size_t arr_size, const string_t* str);
 
 #endif //HTML_PARSER_MISC_H
--- src/misc.c
+++ src/misc.c
@@ -3,8 +3,42 @@
 #include <stdio.h>
 #include <string.h>
 
+int parser_string_init(string_t* str) {
+    str->data = calloc(DEFAULT_CAPACITY, sizeof(char));
+    if (!str->data) {
+        fprintf(stderr, "Out of memory allocating new string");
+        return 1;
+    }
+    str->capacity = DEFAULT_CAPACITY;
+    str->length = 0;
+    return 0;
+}
+
+int parser_string_init_sized(string_t* str, const size_t capacity) {
+    str->data = calloc(capacity, sizeof(char));
+    if (!str->data) {
+        fprintf(stderr, "Out of memory allocating new string");
+        return 1;
+    }
+    str->capacity = capacity;
+    str->length = 0;
+    return 0;
+}
+
+int parser_string_clear(string_t* str) {
+    str->length = 0;
+    return 0;
+}
+
+int parser_string_delete(string_t* str) {
+    free(str->data);
+    str->capacity = 0;
+    str->length = 0;
+    return 0;
+}
+
 int parser_strcmp(const string_t* str1, const string_t* str2) {
-    const size_t len = parser_umin(str1->length, str2->length);
+    const size_t len = parser_min(str1->length, str2->length);
     for (size_t i = 0; i < len; i++) {
         if (str1->data[i] != str2->data[i])
             return str1->data[i] - str2->data[i];
@@ -17,7 +51,7 @@ int parser_strcmp(const string_t* str1, const string_t* str2) {
 }
 
 int parser_cstrcmp(const char* str1, const string_t* str2) {
-    const size_t len = parser_umin(strlen(str1), str2->length);
+    const size_t len = parser_min(strlen(str1), str2->length);
     for (size_t i = 0; i < len; i++) {
         if (str2->data[i] > CHAR_MAX)
             return 1;
@@ -39,7 +73,7 @@ bool contains(const char* arr[], const size_t arr_size, const string_t* str) {
     return false;
 }
 
-int append_char(string_t* str, const int c) {
+int parser_string_append_char(string_t* str, const int c) {
     if (str->length + 1 >= str->capacity) {
         const size_t new_capacity = (str->capacity == 0) ? DEFAULT_CAPACITY : str->capacity * CAPACITY_EXPANSION_COEF;
         int* new_data = realloc(str->data, new_capacity * sizeof(int));
