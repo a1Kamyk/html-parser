833c78457db6aff70ca43f1acaf40f408116550d
a1Kamyk <piotr.kamianka@onet.pl>
2025-11-23 18:44:33 +0100

    added miscellaneous attribute list and string functions

--- include/misc.h
+++ include/misc.h
@@ -4,7 +4,22 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define DEFAULT_CAPACITY 8
+#define NULL_CHARACTER          0x0000
+#define CHARACTER_TABULATION    0x0009
+#define LINE_FEED               0x000A
+#define FORM_FEED               0x000C
+#define CARRIAGE_RETURN         0x000D
+#define SPACE                   0x0020
+
+#define REPLACEMENT_CHARACTER   0xFFFD
+#define SOLIDUS                 0x002F
+#define LESS_THAN_SIGN          0x003C
+#define GREATER_THAN_SIGN       0x003E
+#define QUOTATION_MARK          0x0022
+#define APOSTROPHE              0x0027
+
+#define STRING_DEFAULT_CAPACITY 8
+#define ATTR_LIST_DEFAULT_CAPACITY 4
 #define CAPACITY_EXPANSION_COEF 2
 
 #define parser_max(a, b) (a > b ? a : b)
@@ -22,6 +37,19 @@ static inline bool is_ascii_alpha(const int c) {
     return is_ascii_upper_alpha(c) || is_ascii_lower_alpha(c);
 }
 
+static inline bool is_html_whitespace(int c) {
+    switch (c) {
+        case CHARACTER_TABULATION:
+        case LINE_FEED:
+        case FORM_FEED:
+        case CARRIAGE_RETURN:
+        case SPACE:
+            return true;
+        default:
+            return false;
+    }
+}
+
 typedef struct {
     int*  data;
     size_t length;
@@ -30,11 +58,33 @@ typedef struct {
 
 int parser_string_init(string_t* str);
 int parser_string_init_sized(string_t* str, size_t capacity);
+int parser_string_init_cstr(string_t* str, const char* cstr);
 int parser_string_clear(string_t* str);
 int parser_string_delete(string_t* str);
 int parser_strcmp(const string_t* str1, const string_t* str2);
 int parser_cstrcmp(const char* str1, const string_t* str2);
 int parser_string_append_char(string_t* str, int c);
+int parser_move_string(string_t* dest, string_t* src);
+
+/// Attribute type
+typedef struct {
+    string_t    name;
+    string_t    value;
+} attribute_t;
+
+/// Attribute list
+typedef struct {
+    attribute_t* items;
+    size_t count;
+    size_t capacity;
+} attribute_list_t;
+
+int attribute_list_init(attribute_list_t* list);
+int attribute_list_init_sized(attribute_list_t* list, size_t capacity);
+int attribute_list_clear(attribute_list_t* list);
+int attribute_list_delete(attribute_list_t* list);
+int attribute_list_append(attribute_list_t* list, const attribute_t* item);
+int attribute_list_move(attribute_list_t* dest, attribute_list_t* src);
 
 bool contains(const char* arr[], size_t arr_size, const string_t* str);
 
--- src/misc.c
+++ src/misc.c
@@ -4,24 +4,33 @@
 #include <string.h>
 
 int parser_string_init(string_t* str) {
-    str->data = calloc(DEFAULT_CAPACITY, sizeof(char));
-    if (!str->data) {
-        fprintf(stderr, "Out of memory allocating new string");
-        return 1;
-    }
-    str->capacity = DEFAULT_CAPACITY;
-    str->length = 0;
-    return 0;
+    return parser_string_init_sized(str, STRING_DEFAULT_CAPACITY);
 }
 
 int parser_string_init_sized(string_t* str, const size_t capacity) {
-    str->data = calloc(capacity, sizeof(char));
+    str->data = calloc(capacity, sizeof(int));
     if (!str->data) {
-        fprintf(stderr, "Out of memory allocating new string");
+        fprintf(stderr, "Out of memory allocating new string\n");
         return 1;
     }
     str->capacity = capacity;
     str->length = 0;
+    return 0;
+}
+
+int parser_string_init_cstr(string_t* str, const char* cstr) {
+    if (!cstr)
+        return 1;
+
+    const size_t len = strlen(cstr);
+    if (parser_string_init_sized(str, len) != 0) {
+        return 1;
+    }
+    for (size_t i = 0; i < len; i++) {
+        str->data[i] = (int)(cstr[i]);
+    }
+    str->length = len;
+
     return 0;
 }
 
@@ -65,20 +74,12 @@ int parser_cstrcmp(const char* str1, const string_t* str2) {
     return 0;
 }
 
-bool contains(const char* arr[], const size_t arr_size, const string_t* str) {
-    for (size_t i = 0; i < arr_size; i++) {
-        if (parser_cstrcmp(arr[i], str) == 0) return true;
-    }
-
-    return false;
-}
-
 int parser_string_append_char(string_t* str, const int c) {
-    if (str->length + 1 >= str->capacity) {
-        const size_t new_capacity = (str->capacity == 0) ? DEFAULT_CAPACITY : str->capacity * CAPACITY_EXPANSION_COEF;
+    if (str->length + 1 > str->capacity) {
+        const size_t new_capacity = (str->capacity == 0) ? STRING_DEFAULT_CAPACITY : str->capacity * CAPACITY_EXPANSION_COEF;
         int* new_data = realloc(str->data, new_capacity * sizeof(int));
         if (!new_data) {
-            fprintf(stderr, "Out of memory allocating new string");
+            fprintf(stderr, "Out of memory allocating new string\n");
             return 1;
         }
         str->data = new_data;
@@ -87,4 +88,88 @@ int parser_string_append_char(string_t* str, const int c) {
 
     str->data[str->length++] = c;
     return 0;
+}
+
+int parser_move_string(string_t* dest, string_t* src) {
+    if (!dest || !src)
+        return 1;
+    *dest = *src;
+
+    src->data = NULL;
+    src->capacity = 0;
+    src->length = 0;
+
+    return 0;
+}
+
+int attribute_list_init(attribute_list_t* list) {
+    return attribute_list_init_sized(list, ATTR_LIST_DEFAULT_CAPACITY);
+}
+
+int attribute_list_init_sized(attribute_list_t* list, const size_t capacity) {
+    list->items = calloc(capacity, sizeof(attribute_t));
+    if (!list->items) {
+        fprintf(stderr, "Out of memory allocating new attribute list\n");
+        return 1;
+    }
+    list->capacity = capacity;
+    list->count = 0;
+    return 0;
+}
+
+int attribute_list_clear(attribute_list_t* list) {
+    list->count = 0;
+    return 0;
+}
+
+int attribute_list_delete(attribute_list_t* list) {
+    if (!list) return 0;
+    for (size_t i = 0; i < list->count ; i++) {
+        attribute_t* item = &list->items[i];
+        parser_string_delete(&item->name);
+        parser_string_delete(&item->value);
+    }
+    free(list->items);
+    list->capacity = 0;
+    list->count = 0;
+    return 0;
+}
+
+int attribute_list_append(attribute_list_t* list, const attribute_t* item) {
+    if (list->count + 1 > list->capacity) {
+        const size_t new_capacity = (list->capacity == 0) ?
+            ATTR_LIST_DEFAULT_CAPACITY :
+            list->capacity * CAPACITY_EXPANSION_COEF;
+        attribute_t* new_items = realloc(list->items, new_capacity * sizeof(attribute_t));
+        if (!new_items) {
+            fprintf(stderr, "Out of memory allocating new attribute list\n");
+            return 1;
+        }
+        list->items = new_items;
+        list->capacity = new_capacity;
+    }
+
+    list->items[list->count++] = *item;
+    return 0;
+}
+
+int attribute_list_move(attribute_list_t* dest, attribute_list_t* src) {
+    if (!dest || !src)
+        return 1;
+
+    *dest = *src;
+
+    src->items = NULL;
+    src->capacity = 0;
+    src->count = 0;
+
+    return 0;
+}
+
+bool contains(const char* arr[], const size_t arr_size, const string_t* str) {
+    for (size_t i = 0; i < arr_size; i++) {
+        if (parser_cstrcmp(arr[i], str) == 0) return true;
+    }
+
+    return false;
 }
